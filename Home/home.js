dfede8ad29d61c75c2a0174fa0c87e290dea8d7d
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2018-09-20 14:54:54 +0100

    split into home page

---
+++ Home/home.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableHighlight, FlatList } from 'react-native';
+
+const list = [
+        {
+            key: 0,
+            name: 'Amy Farha'     
+        },
+        {
+            key:1,
+            name: 'Chris Jackson'
+        }
+    ]; 
+
+class HomePage extends React.Component {
+  
+    renderRow ({ item }) {
+        return (
+        <View>
+            <Text style={styles.titleText}>Central London</Text>
+            <Image
+            source={require('../imgs/Central-London.png')} 
+            style={styles.image}
+        />
+        <TouchableHighlight
+         style={styles.button}
+        >
+         <Text style={styles.text}>Start walk</Text>
+        </TouchableHighlight> 
+        </View> )
+    }
+
+    render () {
+        return (
+           <View style={styles.container}> 
+              <FlatList horizontal 
+                data={list}
+                renderItem={this.renderRow}
+              />
+           </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width:'95%',
+    }, 
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#b84c5c',
+        padding: 10,
+        margin:10,
+        width:'95%',
+        borderRadius:5
+    },
+    text: {
+        fontSize:20,
+        color:'#fff'
+    },
+    titleText: {
+        fontSize:20,
+        fontWeight:'bold',
+        margin:10,
+        alignSelf:'flex-start'
+    }
+});
+
+export default HomePage; 
