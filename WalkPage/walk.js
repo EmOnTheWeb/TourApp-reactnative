07ebbabaa8a8a4326e1df1f53df4bd1dae790e53
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2018-09-22 10:21:35 +0100

    get coordinates importing

--- WalkPage/walk.js
+++ WalkPage/walk.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 import { StyleSheet, Text, View, Image, TouchableHighlight, FlatList } from 'react-native';
+import coordinates from '../coordinates'; 
 
 class WalkMap extends React.Component {
 
     componentWillMount() {
 
-        let walkName= this.props.name; 
-        let folderName = walkName.replace(' ','_'); 
-       
-        let coordinates = require('../walks/Central_London/coordinates.json');  
-          
+        let walkName = this.props.name; 
+        let folderName = walkName.replace(' ','_').toLowerCase(); 
+
+        let walkCoordinates = coordinates[folderName]; 
+        console.log(walkCoordinates); 
     }
 
     render() {
+
+           
 
         return (
             <View></View>
