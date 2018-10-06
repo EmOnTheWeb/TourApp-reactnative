267cc44929684094b0a1d66786b8010a10a304e9
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2018-10-06 20:33:37 +0100

    add a description. trying to flex wrap

--- HomePage/home.js
+++ HomePage/home.js
@@ -5,10 +5,12 @@ import { StyleSheet, Text, View, Image, TouchableHighlight, FlatList } from 'rea
 const list = [
         {
             name: 'Central London',
+            description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt',
             image: require('../assets/imgs/Central-London.png')    
         },
         {
             name: 'East London',
+            description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt',
             image: require('../assets/imgs/Central-London.png')
         }
     ]; 
@@ -28,6 +30,7 @@ class HomePage extends React.Component {
 	            source={item.image}
 	            style={styles.image}
         	/>
+            <Text style={styles.descriptionText}>{item.description}</Text>
 	        <TouchableHighlight
 	         	style={styles.button}
 	         	onPress={() => navigator.navigate('Walk', {
@@ -58,9 +61,11 @@ const styles = StyleSheet.create({
     
     container: {
         flex: 1,
+        flexDirection:'column', 
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        flexWrap:'wrap'
     },
     image: {
       
@@ -82,6 +87,10 @@ const styles = StyleSheet.create({
         fontWeight:'bold',
         margin:10,
         alignSelf:'flex-start'
+    },
+    descriptionText: {
+        fontSize:15,
+        margin:10
     }
 });
 
