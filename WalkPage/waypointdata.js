bdcc0b8c2a4541aecacc52440de17171ba238ded
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2018-10-24 16:20:24 +0100

    add kentish town data

--- WalkPage/waypointdata.js
+++ WalkPage/waypointdata.js
@@ -1,6 +1,55 @@
 
 export function requireWaypointImgs(walkName) {
 
+	if(walkName === "kentish_town") {
+
+		return {
+			waypoint_1: {
+				img_src:'waypoint_1.png',
+				title:'Regents Canal' 
+			},
+			waypoint_2: {
+				img_src:'waypoint_2.png',
+				title:'Under Quinn\'s Public House' 
+			},
+			waypoint_3: {
+				img_src:'waypoint_3.png',
+				title:'South Kentish Town Tube Station' 
+			},
+			waypoint_4: {
+				img_src:'waypoint_4.png',
+				title:'The Abbey Tavern'
+			},
+			waypoint_5: {
+				img_src:'waypoint_5.png',
+				title:'Anglers Lane'
+			},
+			waypoint_6: {
+				img_src:'waypoint_6.png',
+				title:'Public Bath House'
+			},
+			waypoint_7: {
+				img_src:'waypoint_7.png',
+				title:'Bath House Signage'
+			},
+			waypoint_8: {
+				img_src:'waypoint_8.png',
+				title:'Queen\'s Crescent Market'
+			},
+			waypoint_9: {
+				img_src:'waypoint_9.png',
+				title:'The Roundhouse'
+			},
+			waypoint_10: {
+				img_src:'waypoint_10.png',
+				title:'Primrose Hill'
+			}
+		}
+	}
+
+
+
+
 	if(walkName === "st_johns_wood") {
 
 		return {
